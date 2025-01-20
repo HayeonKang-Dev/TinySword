cd7322d4583d4b77d515c822160ffe8312aac554
HayeonKang-Dev <hayeon0250@gmail.com>
2025-01-20 17:32:58 +0900

    Create & Update Goblin class
    - Goblin class 생성
    - MoveRight, UpDown, Attack 구현
    - Health, Money, TagId 변수 선언, 조작 함수 구현
    - OverlapEvent 처리 함수 선언

---
+++ TinySword/Source/TinySword/Goblin.cpp
@@ -0,0 +1,176 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Goblin.h"
+#include "Components/CapsuleComponent.h"
+#include "Engine/World.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Character.h"
+#include "PaperFlipbookComponent.h"
+
+
+void AGoblin::BeginPlay()
+{
+    Super::BeginPlay(); 
+
+    Health = MaxHealth; 
+    Money = 0; 
+
+    OverlapComponent = FindComponentByClass<UCapsuleComponent>(); 
+
+    Timer = 0.0f; 
+}
+
+
+void AGoblin::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    
+}
+
+// Control Character
+void AGoblin::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAction(TEXT("Attack"), EInputEvent::IE_Pressed, this, &AGoblin::Attack);
+
+    PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AGoblin::MoveRight);
+    PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AGoblin::UpDown);
+}
+
+void AGoblin::MoveRight(float Value)
+{
+    if (Value != 0.0f)
+    {
+        AddMovementInput(FVector(1.0f, 0.0f, 0.0f), Value);
+    }
+}
+
+void AGoblin::UpDown(float Value)
+{
+    if (Value != 0.0f)
+    {
+        AddMovementInput(FVector(0.0f, 1.0f, 0.0f), Value);
+    }
+}
+
+void AGoblin::FlipCharacter(bool MoveDirec)
+{
+    UPaperFlipbookComponent* SpriteComponent = FindComponentByClass<UPaperFlipbookComponent>(); 
+
+    if (SpriteComponent)
+    {
+        if (MoveDirec) SpriteComponent->SetRelativeScale3D(FVector(1.0f, 1.0f, 1.0f));
+        else SpriteComponent->SetRelativeScale3D(FVector(-1.0f, 1.0f, 1.0f));
+    }
+}
+
+
+//////////////
+
+//////////////
+void AGoblin::NotifyActorBeginOverlap(AActor *OtherActor)
+{
+    Super::NotifyActorBeginOverlap(OtherActor);
+}
+
+void AGoblin::NotifyActorEndOverlap(AActor *OtherActor)
+{
+    Super::NotifyActorEndOverlap(OtherActor);
+}
+
+/////////////
+
+/////////////////
+float AGoblin::TakeDamage(float DamageAmount, FDamageEvent const &DamageEvent, AController *EventInstigator, AActor *DamageCauser)
+{
+    return 0.0f;
+}
+
+bool AGoblin::IsDead() const
+{
+    return Health <= 0;
+}
+
+void AGoblin::Attack()
+{
+    IsAttack = true; 
+    FVector Start, End;
+
+    // Get Dynamite's collision
+    USphereComponent* SphereCollision = FindComponentByClass<USphereComponent>();
+    if (SphereCollision)
+    {
+        float Radius = SphereCollision->GetUnscaledSphereRadius();
+        Start = SphereCollision->GetComponentLocation();
+        Start.X -= Radius; 
+        End = Start; 
+        End.X += Radius; 
+    }
+
+    FHitResult HitResult; 
+    FCollisionQueryParams CollisionParams; 
+    CollisionParams.AddIgnoredActor(this);
+
+    // Line Trace
+    if (GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECollisionChannel::ECC_Visibility, CollisionParams))
+    {
+        AActor* HitActor = Cast<AActor>(HitResult.GetActor());
+        if (HitActor)
+        {
+            FDamageEvent DamageEvent; 
+            HitActor->TakeDamage(Damage, DamageEvent, nullptr, this);
+
+            if (AGoblin* targetPlayer = Cast<AGoblin>(HitActor))
+            {
+
+            }
+
+            // else if .. != my castle, my bomb 
+        }
+    }
+
+}
+
+//////////////////
+
+// Control Variable
+void AGoblin::IncreaseMoney(float Amount)
+{
+    Money += Amount; 
+}
+
+bool AGoblin::DecreaseMoney(float Amount)
+{
+    if (Money < 10) return false;
+    Money -= Amount;
+    return true;
+}
+
+void AGoblin::IncreaseHealth(float Amount)
+{
+    if (Health+Amount > MaxHealth) Health = MaxHealth;
+    else Health += Amount; 
+}
+/////////////
+
+
+
+// Connection with UI
+float AGoblin::GetHealthPercent() const
+{
+    return Health / MaxHealth;
+}
+
+int AGoblin::GetMoneyCount() const
+{
+    return Money;
+}
+
+
+void AGoblin::HandleDeath()
+{
+
+}
---
+++ TinySword/Source/TinySword/Goblin.h
@@ -0,0 +1,94 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/DamageEvents.h"
+#include "PaperZDCharacter.h"
+#include "Goblin.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYSWORD_API AGoblin : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	// override
+	virtual void Tick(float DeltaTime) override; 
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+	// Control variable
+	void IncreaseMoney(float Amount);
+
+	void IncreaseHealth(float Amount);
+
+	bool DecreaseMoney(float Amount);
+
+	// variable
+	UPROPERTY(EditDefaultsOnly)
+	float MaxHealth = 100.0f; 
+
+	UPROPERTY(EditAnywhere, Category="State")
+	float Health;
+
+	UFUNCTION(BlueprintPure)
+	bool IsDead() const;
+
+	UPROPERTY(VisibleAnywhere, Category="State")
+	float Money; 
+
+	UPROPERTY(EditAnywhere, Category="Combat")
+	bool IsAttack = false; 
+
+	UPROPERTY(EditAnywhere, Category="TagId")
+	int TagId = 0; 
+
+	int32 GetTagId() const {return TagId;}
+	
+	UPROPERTY(VisibleAnywhere)
+	UCapsuleComponent* OverlapComponent;
+
+	// for UI 
+	UFUNCTION(BlueprintCallable)
+	float GetHealth() const {return Health;}
+
+	UFUNCTION(BlueprintPure)
+	float GetHealthPercent() const; 
+
+	UFUNCTION(BlueprintPure)
+	int GetMoneyCount() const; 
+
+
+	// Actions
+	UFUNCTION()
+	void Attack();
+
+	void MoveRight(float Value);
+	void UpDown(float Value);
+
+	void HandleDeath();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	virtual void NotifyActorEndOverlap(AActor* OtherActor) override;
+
+	UPROPERTY(EditAnywhere)
+	float Damage = 10; 
+
+private:
+	void FlipCharacter(bool MoveDirec);
+
+	// ATinySwordGameModeBase* GameMode; 
+
+	float Timer; 
+
+};
