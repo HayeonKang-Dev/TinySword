6d14a8ecb661703064ae75b931ad897da5bca076
HayeonKang-Dev <hayeon0250@gmail.com>
2025-01-21 13:48:58 +0900

    Update BaseAISheep
    - 변수 초기화
    - TakeDamage 구현
    - SetupPlayerInputComponent 구현 (입력 연결)
    - IsDead 구현
    - SpawnMeat 구현

--- TinySword/Source/TinySword/BaseAISheep.cpp
+++ TinySword/Source/TinySword/BaseAISheep.cpp
@@ -2,4 +2,86 @@
 
 
 #include "BaseAISheep.h"
+#include "BaseMeat.h"
+#include "TinySwordGameMode.h"
 
+
+void ABaseAISheep::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Health = MaxHealth; 
+    GameMode = Cast<ATinySwordGameMode>(GetWorld()->GetAuthGameMode());
+
+    GameMode->ActiveSheepId.Add(this, GetTagId());
+}
+
+// void ABaseAISheep::Tick(float DeltaTime)
+// {
+//     Super::Tick(float DeltaTime);
+// }
+
+float ABaseAISheep::TakeDamage(float DamageAmount, FDamageEvent const &DamageEvent, AController *EventInstigator, AActor *DamageCauser)
+{
+    float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+    ActualDamage = FMath::Min(Health, ActualDamage);
+    FVector ActorLocation = GetActorLocation(); 
+    TSubclassOf<ABaseMeat> Meat = ABaseMeat::StaticClass(); 
+    if (!IsDead())
+    {
+        Health -= ActualDamage; 
+        if (IsDead())
+        {
+            SpawnMeat();
+        }
+    }
+    return ActualDamage;
+}
+
+// Move 
+void ABaseAISheep::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ABaseAISheep::MoveRight);
+}
+
+
+void ABaseAISheep::MoveRight(float AxisValue)
+{
+    AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
+
+bool ABaseAISheep::IsDead() const
+{
+    return Health <= 0;
+}
+
+void ABaseAISheep::SpawnMeat()
+{
+    UObject* SpawnActor = StaticLoadObject(UObject::StaticClass(), NULL, TEXT("/Game/Blueprints/BP_Meat.BP_Meat")); 
+    UBlueprint* GeneratedBP = Cast<UBlueprint>(SpawnActor);
+    UWorld* World = GetWorld(); 
+
+    if (!SpawnActor || !GeneratedBP || !GeneratedBP->GeneratedClass || !World) return;
+
+    FActorSpawnParameters SpawnParams; 
+	SpawnParams.Owner = this;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+    // Adjust Spawn Location (Lower Z)
+    FVector SpawnLocation = GetActorLocation(); 
+    SpawnLocation.Z = 45.f;
+
+    // Spawn Meat & Set Meat TagId
+    AActor* SpawnedActor; 
+	SpawnedActor = World->SpawnActor<AActor>(GeneratedBP->GeneratedClass, SpawnLocation, GetActorRotation(), SpawnParams);
+    ABaseMeat* SpawnedMeat = Cast<ABaseMeat>(SpawnedActor);
+
+    if (SpawnedMeat)
+    {
+        SpawnedMeat->SetTagId(GetTagId()); 
+        GameMode->ActiveMeatId.Add(SpawnedMeat, GetTagId());
+    }
+
+}
--- TinySword/Source/TinySword/BaseAISheep.h
+++ TinySword/Source/TinySword/BaseAISheep.h
@@ -9,9 +9,49 @@
 /**
  * 
  */
+class ATinySwordGameMode;
+
 UCLASS()
 class TINYSWORD_API ABaseAISheep : public APaperCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	// virtual void Tick(float DeltaTime) override;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void TakeDamageFrom(AActor* Attacker);
+
+	bool IsDead() const;
+
+	void SpawnMeat(); 
+
+	int32 GetTagId() const {return TagId;}
+
+protected:
+	virtual void BeginPlay() override;
+
+	FTimerHandle SpeedBoostTimerHandle; 
+
+	void ResetSpeed(); 
+
+private:
+	void MoveRight(float AxisValue); 
+
+	ATinySwordGameMode* GameMode;
+
+	float BoostedSpeed = 100.0f; 
+
+	float NormalSpeed = 30.0f; 
+
+	float SpeedBoostDuration = 3.f; 
+
+	int32 TagId; 
+
+	float Health; 
+
+	float MaxHealth = 100; 
 };
