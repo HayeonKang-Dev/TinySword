e4281b676d3bd53fa4746a9107c19451f20ee0eb
HayeonKang-Dev <hayeon0250@gmail.com>
2025-01-21 11:34:34 +0900

    Update TinySwordGameMode
    - 게임 내에 사용되는 아이템 관리를 위한 TMap, TQueue 선언

--- TinySword/Source/TinySword/TinySwordGameMode.h
+++ TinySword/Source/TinySword/TinySwordGameMode.h
@@ -4,11 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
+#include <queue>
 #include "TinySwordGameMode.generated.h"
 
 /**
  * 
  */
+
+class ABaseGoldBag; 
+
 UCLASS()
 class TINYSWORD_API ATinySwordGameMode : public AGameMode
 {
@@ -16,6 +20,12 @@ class TINYSWORD_API ATinySwordGameMode : public AGameMode
 	
 public:
 	virtual void Tick(float DeltaTime) override;
+
+	TMap<ABaseGoldBag*, int32> ActiveGoldBagId; 
+	TQueue<int32> ReuseGoldBagId;
+	ABaseGoldBag* FindGoldBagById(const TMap<ABaseGoldBag*, int32>&Map, int32 TargetId);
+
+	TMap<int32, FVector> CastleMap;
 
 protected:
 	virtual void BeginPlay() override; 
