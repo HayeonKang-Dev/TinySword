e2c0ed34235da90cb557c08fe76f802897ca1ab4
HayeonKang-Dev <hayeon0250@gmail.com>
2025-01-24 14:32:49 +0900

    TinySwordPlayerController 클래스 생성

---
+++ TinySword/Source/TinySword/TinySwordPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TinySwordPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYSWORD_API ATinySwordPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
