de141d15edeb31d595cbf6fb5b4e76a0627529ce
HayeonKang-Dev <hayeon0250@gmail.com>
2025-01-24 15:13:40 +0900

    Playing 위젯 코드 수정
    - 위젯 초기화 : playerController, GameMode, controlledChar 참조 생성
    - OnSpawnButtonClicked : Spawn 버튼 클릭 시 동작 구현
    - DecreasePlayerMoney : 폭탄 스폰 전에 돈 감소시키기
    - spawnBomb : 버튼을 클릭한 플레이어와 같은 색상의 성에서 폭탄 스폰
    - SetBombIndex : 폭탄 스폰 시 인덱스 값 부여

--- TinySword/Source/TinySword/PlayingWidget.cpp
+++ TinySword/Source/TinySword/PlayingWidget.cpp
@@ -2,4 +2,99 @@
 
 
 #include "PlayingWidget.h"
+#include "Blueprint/UserWidget.h"
+#include "Goblin.h"
+#include "Kismet/GameplayStatics.h"
+#include "TinySwordPlayerController.h"
+#include "TinySwordGameMode.h"
+#include "BaseBomb.h"
 
+void UPlayingWidget::NativeConstruct()
+{
+    Super::NativeConstruct(); 
+
+    playerController = Cast<ATinySwordPlayerController>(GetOwningPlayer()); 
+    GameMode = Cast<ATinySwordGameMode>(UGameplayStatics::GetGameMode(GetWorld()));
+    controlledChar = Cast<AGoblin>(playerController->GetPawn());
+
+    if (SpawnButton) SpawnButton->OnClicked.AddDynamic(this, &UPlayingWidget::OnSpawnButtonClicked);    
+}
+
+void UPlayingWidget::OnSpawnButtonClicked()
+{
+    if (DecreasePlayerMoney())
+    {
+        SpawnBomb();
+    }
+}
+
+bool UPlayingWidget::DecreasePlayerMoney()
+{
+    if (playerController && controlledChar) return controlledChar->DecreaseMoney(10);
+    return false;
+}
+
+void UPlayingWidget::SpawnBomb()
+{
+    if (GameMode)
+    {
+        TMap<int32, FVector>& CastleMap = GameMode->GetCastleMap();
+        UObject* spawnActor = nullptr; 
+        switch (controlledChar->GetTagId())
+        {
+            case 0: 
+                spawnActor = StaticLoadObject(UObject::StaticClass(), NULL, TEXT("/Script/Engine.Blueprint'/Game/Blueprints/Bombs/BP_Bomb.BP_Bomb'"));
+                break;
+
+            case 1: 
+                spawnActor = StaticLoadObject(UObject::StaticClass(), NULL, TEXT("/Script/Engine.Blueprint'/Game/Blueprints/Bombs/BP_Bomb_Purple.BP_Bomb_Purple'"));
+                break;
+
+            case 2: 
+                spawnActor = StaticLoadObject(UObject::StaticClass(), NULL, TEXT("/Script/Engine.Blueprint'/Game/Blueprints/Bombs/BP_Bomb_Red.BP_Bomb_Red'"));
+                break;
+
+            case 3: 
+                spawnActor = StaticLoadObject(UObject::StaticClass(), NULL, TEXT("/Script/Engine.Blueprint'/Game/Blueprints/Bombs/BP_Bomb_Yellow.BP_Bomb_Yellow'"));
+                break;
+
+            default: 
+                break;
+        }
+
+        UBlueprint* GeneratedBP = Cast<UBlueprint>(spawnActor); 
+        UWorld* World = GetWorld(); 
+        if (!spawnActor || !GeneratedBP || !GeneratedBP->GeneratedClass || !World) return; 
+
+        FActorSpawnParameters SpawnParams; 
+        SpawnParams.Owner = controlledChar; 
+        SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn; 
+
+        FVector* FoundLocation = CastleMap.Find(controlledChar->GetTagId()); 
+        if(FoundLocation)
+        {
+            SpawnLocation = *FoundLocation; 
+            SpawnLocation.Z = 73.0f; 
+            SpawnLocation.Y += 80.0f;
+            
+            AActor* OwnerActor = controlledChar; 
+            AActor* SpawnedActor = World->SpawnActor<AActor>(GeneratedBP->GeneratedClass, SpawnLocation, OwnerActor->GetActorRotation(), SpawnParams);
+            
+            
+            if (SpawnedActor)
+            {
+                SpawnedBomb = Cast<ABaseBomb>(SpawnedActor); 
+                if (SpawnedBomb) SetBombIndex();
+            }
+        }
+    }
+}
+
+void UPlayingWidget::SetBombIndex()
+{
+    int32 id = 0; 
+    if (!GameMode->ReuseBombId.IsEmpty()) GameMode->ReuseBombId.Dequeue(id);
+    else id = GameMode->ActiveBombId.Num() + 1; 
+    SpawnedBomb->SetTagId(id); 
+    GameMode->ActiveBombId.Add(SpawnedBomb, id);
+}
--- TinySword/Source/TinySword/PlayingWidget.h
+++ TinySword/Source/TinySword/PlayingWidget.h
@@ -4,14 +4,46 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
 #include "PlayingWidget.generated.h"
+
 
 /**
  * 
  */
+class ATinySwordGameMode;
+class ATinySwordPlayerController; 
+class AGoblin;
+class ABaseBomb;
+
 UCLASS()
 class TINYSWORD_API UPlayingWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public: 
+	virtual void NativeConstruct() override; 
+
+protected: 
+	UPROPERTY(meta = (BindWidget))
+	class UButton* SpawnButton;
+
+	void OnSpawnButtonClicked(); 
+
+	bool DecreasePlayerMoney();
+
+	void SpawnBomb(); 
+
+	void SetBombIndex(); 
+
+private:
+	ATinySwordGameMode* GameMode; 
+
+	ATinySwordPlayerController* playerController; 
+
+	AGoblin* controlledChar;
+
+	FVector SpawnLocation;
+
+	ABaseBomb* SpawnedBomb;
 };
