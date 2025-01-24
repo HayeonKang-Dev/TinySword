6ef5ae154b32411ff7b317bb1ec5bb1f3517afd2
HayeonKang-Dev <hayeon0250@gmail.com>
2025-01-24 11:34:52 +0900

    Bomb 피해, 공격 구현
    - BeginPlay 초기화
    - TakeDamage 구현
    - 폭탄이므로, RadialDamage 구현
    - IsDead : 폭탄 사망 여부 확인
    - setTagId : 폭탄 생성 시 TagId 부여

--- TinySword/Source/TinySword/BaseBomb.cpp
+++ TinySword/Source/TinySword/BaseBomb.cpp
@@ -2,4 +2,87 @@
 
 
 #include "BaseBomb.h"
+#include "Goblin.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
+
+void ABaseBomb::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    Health = MaxHealth; 
+    GetCharacterMovement()->MaxWalkSpeed = Speed; 
+    Damage = 10.0f; 
+
+    bombController = Cast<ABombAIController>(GetController());
+    GameMode = Cast<ATinySwordGameMode>(GetWorld()->GetAuthGameMode());
+}
+
+
+
+void ABaseBomb::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+
+
+float ABaseBomb::TakeDamage(float DamageAmount, FDamageEvent const &DamageEvent, AController *EventInstigator, AActor *DamageCauser)
+{
+    if (DamageCauser && DamageCauser->IsA(ABaseBomb::StaticClass())) return 0.0f; 
+    
+    if (AGoblin* Causer = Cast<AGoblin>(DamageCauser))
+    {
+        if (Causer && Causer->GetTagId() == TagId) return 0.0f; 
+    }
+
+    float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser); 
+    if(!IsDead())
+    {
+        ActualDamage = FMath::Min(Health, ActualDamage); 
+        Health -= ActualDamage; 
+
+        if (IsDead())
+        {
+            
+        }
+    }
+    return ActualDamage;
+}
+
+
+void ABaseBomb::DealRadialDamage()
+{
+    FVector Origin = GetActorLocation(); 
+    float DamageRadius = 100.0f; 
+    TArray<AActor*> IgnoreActors;
+    AController* InstigatedByController = nullptr;
+
+    TArray<AActor*> DamagedActors; 
+
+    UGameplayStatics::ApplyRadialDamage(
+        this,
+        Damage, 
+        Origin, 
+        DamageRadius, 
+        UDamageType::StaticClass(),
+        DamagedActors, 
+        this, 
+        GetInstigatorController(), 
+        true
+    );
+}
+
+
+bool ABaseBomb::IsDead() const
+{
+    return Health <= 0;
+}
+
+
+
+void ABaseBomb::SetTagId(int32 newTagId)
+{
+    TagId = newTagId;
+}
--- TinySword/Source/TinySword/BaseBomb.h
+++ TinySword/Source/TinySword/BaseBomb.h
@@ -4,14 +4,60 @@
 
 #include "CoreMinimal.h"
 #include "PaperCharacter.h"
+#include "BombAIController.h"
+#include "TinySwordGameMode.h"
 #include "BaseBomb.generated.h"
 
 /**
  * 
  */
+// class ABombAIController;
+// class ATinySwordGameMode;
+
+
 UCLASS()
 class TINYSWORD_API ABaseBomb : public APaperCharacter
 {
 	GENERATED_BODY()
-	
+
+public: 
+	bool IsDead() const; 
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	void DealRadialDamage();
+
+	float GetHealth() const {return Health;}
+
+	int32 GetTagId() const {return TagId;}
+
+	void SetTagId(int32 newTagId);
+
+protected:
+	virtual void BeginPlay() override; 
+
+	FTimerHandle BombExplosionTimerHandle; 
+
+	float ElapsedTime = 0.0f; 
+
+private:
+	UPROPERTY(VisibleAnywhere, Category="State")
+	float Health; 
+
+	float MaxHealth = 70.0f;
+
+	UPROPERTY(VisibleAnywhere, Category="State")
+	float Speed = 50.0f; 
+
+	UPROPERTY(VisibleAnywhere, Category="State")
+	float Damage; 
+
+	UPROPERTY(VisibleAnywhere, Category="State")
+	int32 TagId;
+
+	ABombAIController* bombController; 
+
+	ATinySwordGameMode* GameMode;
 };
