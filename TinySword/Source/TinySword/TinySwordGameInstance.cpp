c3c791fcfad5cc8f4b1a834a89f9e9b25bd6056c
HayeonKang-Dev <hayeon0250@gmail.com>
2025-03-27 17:14:11 +0900

    Init 구현
    - GameInstance를 싱글톤으로 구현
    - Init
        - TCPClient 생성
        - TCPClient에서 Game Instance, GameMode 참조하도록 World 전달

--- TinySword/Source/TinySword/TinySwordGameInstance.cpp
+++ TinySword/Source/TinySword/TinySwordGameInstance.cpp
@@ -2,4 +2,79 @@
 
 
 #include "TinySwordGameInstance.h"
+#include "Kismet/GameplayStatics.h"
 
+// void UTinySwordGameInstance::Init()
+// {
+    
+//     Super::Init();
+//     cnt++;
+//     UE_LOG(LogTemp, Warning, TEXT("Game Instance - INIT : %d"), cnt);
+    
+
+//     if (!TCPClient.IsValid())
+//     {
+//         UE_LOG(LogTemp, Warning, TEXT("MakeShared TCPClient"));
+
+//         TCPClient = MakeShared<FTCPSocketClient_Async>();
+        
+//         if (TCPClient.IsValid() && !bIsConnect)
+//         {
+//             TCPClient->Connect();
+//             UE_LOG(LogTemp, Warning, TEXT("TCPClient->Connect() running."));
+//             bIsConnect = true; 
+
+//             // bHasInitialized = true;
+//         }
+//         else
+//         {
+//             UE_LOG(LogTemp, Warning, TEXT("TCPClient is not VALID"));
+//         }
+        
+//     }
+//     else
+//     {
+//         UE_LOG(LogTemp, Warning, TEXT("TCPClient is VALID "));
+//         TCPClient->Connect();
+//     }
+	
+// }
+
+void UTinySwordGameInstance::Init()
+{
+    Super::Init();
+
+    if (bIsConnect)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Game Instance already initialized. Skipping Init()..."));
+        return;
+    }
+
+    cnt++;
+    UE_LOG(LogTemp, Warning, TEXT("Game Instance - INIT : %d"), cnt);
+
+    if (!TCPClient.IsValid())
+    {
+        TCPClient = MakeShared<FTCPSocketClient_Async>();
+        UE_LOG(LogTemp, Warning, TEXT("Created new TCPClient."));
+    }
+
+    if (TCPClient.IsValid())
+    {
+        UWorld* World = GetWorld();
+        // TCPClient->Connect(); /////////////////////////////////////// 실행 됐는데 연결 안됨 
+        TCPClient->Initialize(World);
+        UE_LOG(LogTemp, Warning, TEXT("TCPClient->Connect() running."));
+        bIsConnect = true;
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("TCPClient is not valid."));
+    }
+}
+
+UTinySwordGameInstance *UTinySwordGameInstance::GetInstance(UWorld *World)
+{
+    if (World) return World->GetGameInstance<UTinySwordGameInstance>();
+    return nullptr;
+}
--- TinySword/Source/TinySword/TinySwordGameInstance.h
+++ TinySword/Source/TinySword/TinySwordGameInstance.h
@@ -5,25 +5,48 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "Goblin.h"
+#include "AsyncNetworking.h"
 #include "TinySwordGameInstance.generated.h"
 
 /**
  * 
  */
+
+ class FTCPSocketClient_Async;
+
 UCLASS()
 class TINYSWORD_API UTinySwordGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 
 public: 
+	virtual void Init() override;
+
 	int32 GetTagId() const {return TagId; }
 	void SetTagId(int32 newTagId) {TagId = newTagId;}
 
 	AGoblin* GetChar() const {return controlledChar;}
 	void SetChar(AGoblin* newChar) {controlledChar = newChar;}
 
+	// 싱글톤 접근 함수
+    static UTinySwordGameInstance* GetInstance(UWorld* World);
+
+    // TCPClient에 대한 접근자
+    TSharedPtr<FTCPSocketClient_Async> GetTCPClient()
+    {
+        return TCPClient;
+    }
+
+	
+
 private:
 	int32 TagId; 
 
 	AGoblin* controlledChar; 
+
+	TSharedPtr<FTCPSocketClient_Async> TCPClient;
+
+    bool bIsConnect = false; 
+
+	int cnt = 0;
 };
